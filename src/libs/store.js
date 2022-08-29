26e14c17bee40ddb559f263a7b57b7f96d4ae43f
Bespalov Alexey <bespalov.alexey@yandex.ru>
2022-08-29 12:04:57 +0700

    Убрал лишние переменные из store

--- src/libs/store.js
+++ src/libs/store.js
@@ -2,8 +2,3 @@ import { writable } from 'svelte/store';
 
 export const mParamsStore = writable([])
 export const mColumsStore = writable([])
-export const time_readStore = writable(0)
-export const time_parseStore = writable(0)
-export const time_storeStore = writable(0)
-export const count_colums = writable(0)
-export const count_row = writable(0)
