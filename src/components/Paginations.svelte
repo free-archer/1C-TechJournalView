e32a4f9eb56d7e80f941455c87fc52a142322a8a
Bespalov Alexey <bespalov.alexey@yandex.ru>
2022-08-29 12:24:52 +0700

    #2 Добавил шаблон пагинации

---
+++ src/components/Paginations.svelte
@@ -0,0 +1,20 @@
+<script>
+    let start_paginations = 0
+    let step_paginations = 10
+ </script>
+
+<nav class="pagination" role="navigation" aria-label="pagination">
+    <a class="pagination-previous is-disabled" title="This is the first page">Previous</a>
+    <a class="pagination-next">Next page</a>
+    <ul class="pagination-list">
+      <li>
+        <a class="pagination-link is-current" aria-label="Page 1" aria-current="page">1</a>
+      </li>
+      <li>
+        <a class="pagination-link" aria-label="Goto page 2">2</a>
+      </li>
+      <li>
+        <a class="pagination-link" aria-label="Goto page 3">3</a>
+      </li>
+    </ul>
+</nav>
--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -2,6 +2,11 @@
    import {mParamsStore, mColumsStore} from '../libs/store'
 
    import Cell from "./Cell.svelte";
+   import Paginations from './Paginations.svelte';
+
+   //paginations
+   let start_paginations = 0
+   let step_paginations = 10
 </script>
 
 <div class="table">
@@ -12,7 +17,7 @@
       {/each}  
    </div>
 
-   {#each $mParamsStore as params}
+   {#each $mParamsStore.slice(start_paginations, step_paginations) as params}
    <div class="table-row">
       {#each $mColumsStore as column}
          <Cell paramValue={(params.get(column)) ? params.get(column) : ""}/>
@@ -21,6 +26,8 @@
    {/each}
 
 </div>
+
+<Paginations />
     
 <style>
 .table
