9890eba86d8182012f1ee08353c2fa6949516d33
Bespalov Alexey <bespalov.alexey@yandex.ru>
2022-09-01 23:38:33 +0700

    страница настроек таблицы

--- src/components/settings/SettingsTable.svelte
+++ src/components/settings/SettingsTable.svelte
@@ -1,13 +1,22 @@
 <script>
     import {columnWidth, step_paginations} from '../../libs/store'
+
+    function updateColumnWidth() {
+
+    }
+
+    step_paginations.subscribe(() => {
+        $step_paginations = +$step_paginations
+    })
 </script>
 
 <div class="panel-block is-flex">
     <div class="box is-fles">
     
-        Отметте колонки которые будут отбображаться
+        Ширина столбцов
         <input bind:value={$columnWidth} class="input is-info" type="text" placeholder="Info input">
-        Отметте колонки которые будут отбображаться
+
+        Шаг пагинации
         <input bind:value={$step_paginations} class="input is-info" type="text" placeholder="Info input">
 
     </div>
